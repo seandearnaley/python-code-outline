3362782f324c03e0f5ab4bb6cf7358b5086ef19a
Sean Dearnaley <seandearnaley@hotmail.com>
2023-06-05 02:38:24 -0400

    update init

--- python_code_outline/__init__.py
+++ python_code_outline/__init__.py
@@ -1,2 +1,6 @@
 """This module is the entry point for the python_code_outline package."""
-from .python_report_generator import get_report  # noqa
+from .python_report_generator import get_report
+
+__all__ = [
+    "get_report",
+]
